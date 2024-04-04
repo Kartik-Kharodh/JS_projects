19708e7fab68d1867326933bb0c34a6c9b636701
Kartik-Kharodh <161797166+Kartik-Kharodh@users.noreply.github.com>
2024-04-04 05:01:07 +0000

    datatypes and variables done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // by using it the  JS code will be treated as a whole new version
+
+/*
+just like any other lang it has datatypes like string ,interger,boolean,bigint(for very large int value)
+null = standalone value
+undefined
+symbol =unique
+object
+*/
+    
+let num =2
+let string  = "Name"
+let Myth = true
+let random = null
+let x
+
+console.log(typeof num)
+console.log(typeof string)
+console.log(typeof Myth)
+console.log(typeof random)
+console.log(typeof x)
---
+++ 01_basics/variables.js
@@ -0,0 +1,22 @@
+let accountid = "ksqre6"
+const phoneno = 12345
+var accountgmail = "ksqre6@gmail.com"
+accountcity = "jaipur"
+let random
+
+console.table([accountid,accountcity,accountgmail,random])
+
+accountgmail = "kharodg@gmail.com"
+accountid = "wizard"
+accountcity = "chandigarh"
+
+console.table([accountid,accountcity,accountgmail,random])
+
+/*
+Note : use let instead of var for declaring variables due to the issue of block scope
+and functional scope
+
+also after we declare anything as const we cannot change it afterwards throughout the program
+
+Mainly two documentation to be followed 1) MDN 2)ECMAscript
+*/
