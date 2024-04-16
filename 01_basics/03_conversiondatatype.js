869d875f2d193ca33ec0f01dd34a2cc1c6719101
Kartik-Kharodh <161797166+Kartik-Kharodh@users.noreply.github.com>
2024-04-16 14:09:35 +0530

    03_conversiondatatype.js

--- 01_basics/03_conversiondatatype.js
+++ 01_basics/03_conversiondatatype.js
@@ -22,3 +22,43 @@ let num2 = String(num)
 console.log(typeof(num2));
 console.log(num2);
 // yes it gets converted to string and gives 12 as string
+let somenum  = "abc"
+
+// *********************operations*****************************
+
+let val = 3
+let negval = -val
+console.log(negval); //=> -3
+ 
+let str1 = "Kartik"
+let str2  =" Kharodh"
+let str3 = str1 + str2
+console.log(str3); //=> Kartik Kharodh
+
+//similarly other arthmetic operations can also be done
+console.log(2+4);
+console.log(2-9);
+console.log(2/3);
+console.log(2*3);
+console.log(2**3);
+console.log(2%3);
+
+
+//some more interesting things
+console.log("1" + "2");//=>12
+console.log(1 + "2");//=>12
+console.log("1" + 2);//=>12
+console.log("1" + 2 + 4);//=>124
+console.log(1 + 2 + "5");//=>35
+
+console.log(+true);//this will convert true to number i.e 1
+console.log(+"");//=> 0
+
+let int1,int2,int3
+int1 = int2 = int3 = 5+3
+console.log(int2);
+
+//here we can see the operator precedence
+let x = 4
+console.log(x++); //=>4
+console.log(++x); //=>5
