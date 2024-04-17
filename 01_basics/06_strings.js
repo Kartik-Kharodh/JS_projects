f2bda01cde87bd18ce98a37218528e3309fb3a69
Kartik-Kharodh <161797166+Kartik-Kharodh@users.noreply.github.com>
2024-04-17 11:13:11 +0000

    strings done

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,38 @@
+// let str1  = " hello"
+// let str2 = " Kartik"
+// console.log("hey there" + str1 + str2) //this is an old method 
+
+//new method is 
+// let name = "Kartik"
+// let Branch = "IT"
+// console.log(`hello there my name is ${name} and my Branch is ${Branch}`)
+// NOTE => please use the above syntax instead of  str1 + str2 and others this type syntax
+// another important method to declare string is 
+let str = new String('KartikKharodh')
+//by this method the string will act like an object in which string is stored like key value pair
+//Also has multiple methods which are very useful
+// console.log(str[0]);
+// console.log(str[9]);
+// console.log(str.length);
+// console.log(str.toUpperCase());
+// console.log(str.charAt(11));
+// console.log(str.indexOf('t'));
+
+// let newstr = str.substring(0,7) // also the last index is not included and -ve indexing is not allowd in substring
+// console.log(newstr);
+// let newstr2 = str.slice(-5,13)
+// console.log(newstr2);
+
+// let newstr3 = "     kartik      "
+// console.log(newstr3);
+// console.log(newstr3.trim());//basically it will trim the extra or unwanted spaces 
+
+// let newstr = "hey everyone my name is kartik kharodh"
+// let newstr2 = newstr.split(" ") //this will split the string into a list or array on the basis of given seperator and limit
+// console.log(newstr2);
+
+// NOTE => Read all the different methods of strings which will help in mastering the strings
+
+
+
+
